42f06bf33698b40d2ec7cfb191cda0c59eea3e60
Monica Lisa Reyes <mlisareyes@gmail.com>
2018-06-22 05:02:27 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,5 @@
-def reverse_each_word
-  puts "Hello there, and how are you?"
+def reverse_each_word(sentence)
+  "Hello there, and how are you?"
 end
+
+puts reverse_each_word("Hello there, and how are you?")
